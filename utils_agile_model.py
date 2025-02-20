79460db1906e9d11eba822bd1f2cb25df718fd11
valentin.b <valentin.bordoux@gmail.com>
2025-02-20 11:18:20 +0100

    returning to clean state

--- utils_agile_model.py
+++ utils_agile_model.py
@@ -22,6 +22,11 @@ from chirp.inference.classify import data_lib
 from chirp.models import metrics
 import numpy as np
 import tensorflow as tf
+from ml_collections import config_dict
+from chirp.inference import embed_lib
+import pandas as pd
+import os
+
 
 @dataclasses.dataclass
 class ClassifierMetrics:
@@ -142,4 +147,123 @@ def train_from_index(
 
 
 
-# TODO(valentinB) Function for model selection
+def choose_embedding_model(model_name, models_folder_path="./models/"):
+    '''
+    Load model, corresponding paramaters and run a test according to the model selected.
+    Args:
+        model_name: str. Name of the model to load (surfperch, perch, birdnet)
+        models_folder_path: str. Path to the folder containing the models
+    Returns:
+        embed_fn: Embedding function
+        config: ConfigDict
+    '''
+
+    # Load and test the model corresponding to the name given in input
+    config = config_dict.ConfigDict()
+    embed_fn_config = config_dict.ConfigDict()
+    model_config = config_dict.ConfigDict()
+
+    # SurfPerch
+    if model_name.lower() == 'surfperch':
+        embed_fn_config.model_key = 'taxonomy_model_tf'
+        model_config.window_size_s = 5.0
+        model_config.hop_size_s = 5.0
+        model_config.sample_rate = 32000
+        model_config.model_path = models_folder_path + 'SurfPerch-model/'
+    
+    # Perch
+    elif model_name.lower() == 'perch':
+        embed_fn_config.model_key = 'taxonomy_model_tf'
+        model_config.window_size_s = 5.0
+        model_config.hop_size_s = 5.0
+        model_config.sample_rate = 32000
+        model_config.model_path = models_folder_path + 'Perch-model/'
+
+    # BirdNET
+    elif model_name.lower() == 'birdnet':
+        embed_fn_config.model_key = 'birdnet'
+        model_config.window_size_s = 3.0
+        model_config.hop_size_s = 3.0
+        model_config.sample_rate = 48000
+        model_config.model_path = models_folder_path + 'BirdNET-model/BirdNET_GLOBAL_6K_V2.4_Model_FP32.tflite'
+        model_config.num_tflite_threads = 16
+        model_config.class_list_name = 'birdnet_v2_1'
+
+    # Name error
+    else:
+        print(f"No model corresponding to the input {model_name}, possible choices: surfperch, perch, birdnet")
+        return [], []
+    
+    embed_fn_config.write_embeddings = True
+    embed_fn_config.write_logits = False
+    embed_fn_config.write_separated_audio = False
+    embed_fn_config.write_raw_audio = False
+    config.embed_fn_config = embed_fn_config
+    embed_fn_config.model_config = model_config
+    # Number of parent directories to include in the filename. This allows us to
+    # process raw audio that lives in multiple directories.
+    config.embed_fn_config.file_id_depth = 1
+    
+    # Loading model and testing on zeros
+    # The embed_fn object is a wrapper over the model.
+    embed_fn = embed_lib.EmbedFn(**config.embed_fn_config)
+    print(f'\n\nLoading {model_name}...')
+    embed_fn.setup()
+
+    print('Test-run of model...')
+    z = np.zeros([int(model_config.sample_rate * model_config.window_size_s)])
+    embed_fn.embedding_model.embed(z)
+    print('\nSetup complete!')
+        
+    return embed_fn, config
+
+
+
+# CSV to Raven format
+def eval_to_Raven(prediction_file, wind_dur, keep_only_call=True, freq_bands=[0, 16000]):
+    """ Convert a detections csv file into several files, one file per audio files in the Raven Pro format
+        return None, save all files in a subdirectory "Raven file" where the prediction file is.
+    Args:
+     - prediction_file: path to the file containing the detections
+     - wind_dur: size of the detection window to calculate the end of the annotations
+     - keep_only_call: set to false to keep the annotation of noise class as well
+     - freq_bands: define the frequency range of the annotations in the raven annotation files
+     Output:
+     - None
+      """
+    
+    predicted_events = pd.read_csv(prediction_file, sep=',')
+    if keep_only_call:
+        predicted_events = predicted_events.loc[predicted_events[' label'] != ' Unknown']
+
+    source_dir, filename = os.path.split(prediction_file)
+    raven_dir = os.path.join(source_dir, 'Raven file/')
+    if not os.path.exists(raven_dir):
+        os.makedirs(raven_dir)
+
+    list_predicted_files = predicted_events.groupby(['filename'])
+
+    for _idx, predicted_file  in list_predicted_files:
+
+        raven_file_df = pd.DataFrame({'Begin Time (s)': predicted_file[' timestamp_s'], 'End Time (s)': predicted_file[' timestamp_s']+wind_dur})
+        raven_file_df.index.name = 'Selection'
+        raven_file_df['View'] = 'Spectrogram 1'
+        raven_file_df['Channel'] = 1
+        raven_file_df['Low Freq (Hz)'] = freq_bands[0]
+        raven_file_df['High Freq (Hz)'] = freq_bands[1]
+        raven_file_df['Type'] = predicted_file[' label']
+        # raven_file_df['Logit'] = predicted_file[' logit']
+        raven_file_df['Confidence Score'] = predicted_file[' confidence']*100 # Confidence score to percentage
+
+
+        if raven_file_df.index[0] == 0: # Raven does not support index starting at 0
+            raven_file_df.index += 1
+
+        dest_filepath = raven_dir + os.path.basename(predicted_file['filename'].iloc[0])[:-4] +'.txt'
+        try:
+            raven_file_df.to_csv(dest_filepath, sep='\t')
+            # print('Raven annotation saved at:', dest_filepath)
+        except Exception as e:
+            print("Could not save the annotations in a csv file:", e)
+
+    print("Raven files saved at:", raven_dir)
